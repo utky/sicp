e68c08440f9afd1eb90083f7462684d9e47363ba
utky <ilyaletre@gmail.com>
2020-09-13 23:34:06 +0000

    Q 1.29

--- chapter1.scm
+++ chapter1.scm
@@ -640,3 +640,30 @@
 ;; #t
 ;; gosh> (miller-rabin-prime? 1729 5)
 ;; #f
+
+;; 1.3 高階手続きによる抽象
+(define (inc a) (+ a 1))
+(define (cube x) (* x x x))
+
+(define (sum term a next b)
+  (if (> a b)
+    0
+    (+ (term a)
+       (sum term (next a ) next b))))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx))
+
+;; Q 1.29
+(define (simpson f a b n)
+  (define h (/ (- b a) n))
+  (define (yk k) (f (+ a (* k h))))
+  (define (term k)
+    (cond ((= k 0)               (yk k))
+          ((= k n)               (yk k))
+          ((= (remainder k 2) 0) (* 2 (yk k)))
+          (else                  (* 4 (yk k)))))
+  (* (/ h 3)
+     (sum term 0 inc n)))
